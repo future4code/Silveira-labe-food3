f802abeb57817868e3c4ad3b149ce3810bd7ba6b
graziela-queiroz <99085346+graziela-queiroz@users.noreply.github.com>
2022-05-20 17:53:03 -0300

    ajustes adicionar pedido

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Router from '../src/routes/Router';
 import theme from './constants/theme'
 import { ThemeProvider } from '@material-ui/core/styles' 
+import GlobalState from "./context/global/GlobalState";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Router />
+      <GlobalState>
+        <Router />
+      </GlobalState>
     </ThemeProvider>
   )
 }
--- src/pages/RestaurantDetailsPage/RestaurantCard.jsx
+++ src/pages/RestaurantDetailsPage/RestaurantCard.jsx
@@ -1,9 +1,14 @@
-import { Card, CardContent, CardMedia, Typography, useTheme, makeStyles, Button } from '@material-ui/core';
-import React from 'react'
+import { Card, CardContent, CardMedia, Typography, useTheme, makeStyles, Button, TextField } from '@material-ui/core';
+import React, { useContext } from 'react'
+import { colors } from '@material-ui/core';
+import GlobalStateContext from '../../context/global/GlobalStateContext';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    maxWidth: 345,
+    margin: 20,
+    justifyContent: 'center',
   },
   details: {
     display: 'flex',
@@ -11,52 +16,59 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flex: '1 0 auto',
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
   },
   cover: {
-    width: 151,
+    width: 100,
+    display: 'flex',
+    alignSelf: 'center',
   },
 }));
 
-
-
 const RestaurantCard = (props) => {
- 
   const theme = useTheme();
   const classes = useStyles();
 
+  const { photoUrl, name, description, price, amount ,id} = props.product
+  const { states, setters } = useContext(GlobalStateContext);
+  const { cart, cartOrder } = states;
+  const productsExist = cartOrder.products.some((product)=>id === product.id) 
+  console.log(props.quantidade)
   return (
     <div>
       <Card className={classes.root}>
         <CardMedia className={classes.cover}
           component="img"
           height="140"
-          image={props.photoUrl}
+          image={photoUrl}
           title="Live from space album cover"
         />
         <div className={classes.details}>
           <CardContent className={classes.content}>
-            <Typography component="h6" variant="h6">
-              {props.name}
+            <Typography component="p" style={{color: "#E8222E"}}>
+              {name}
             </Typography>
-            <Typography variant="subtitle3" color="textSecondary">
-              {props.description}
+            <Typography variant="subtitle1" style={{color: "#B8B8B8", fontSize: 12}}>
+              {description}
             </Typography>
-            <div>
-              <Typography variant="subtitle1" color="textSecondary">
-                R${props.price}
-              </Typography>
-            </div>
+            <Typography variant="subtitle1" style={{fontWeight: 'bold', color: "black", lineHeight: '3rem'}}>
+              R${price}
+            </Typography>
           </CardContent>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <Button variant="outlined" color="primary" size="small"  onClick={() => props.modal} style={{ width: 20 }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+          <Typography variant="outlined">qnt: </Typography>
+         {productsExist ?  <Button variant="outlined" color="primary" size="small" 
+          onClick={() => props.removeToCart(id)} style={{ width: 20 }}>
+            remover
+          </Button> :
+          <Button variant="outlined" color="primary" size="small" 
+          onClick={() =>{ props.setCart({id:props.product.id, photoUrl:props.product.photoUrl, name:props.product.name, description:props.product.description, price:props.product.price}); props.abreModal(props.product)}} style={{ width: 20 }}>
             Add
-          </Button>
-          
-          {/* <Typography variant="subtitle1"> '' </Typography>
-          <Button variant="outlined" color="primary" size="small" onClick={() => props.remover} style={{ width: 20}}>
-            -
-          </Button> */}
+          </Button>}
         </div>
       </Card>
     </div>
